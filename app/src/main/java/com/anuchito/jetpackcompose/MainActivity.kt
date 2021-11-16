12bcddd16af2d88ae05c13221cbca07f23f5138f
AnuchitO <anuchit.prasertasng@gmail.com>
2021-11-16 14:08:07 +0700

    Init Project Jetpack Compose

---
+++ app/src/main/java/com/anuchito/jetpackcompose/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.anuchito.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.anuchito.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("AnuchitO")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeTheme {
+        Greeting("AnuchitO!")
+    }
+}
