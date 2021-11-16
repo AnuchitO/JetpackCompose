c98f73061bb96bc67608c2e89aba5c43f71ae8a2
AnuchitO <anuchit.prasertasng@gmail.com>
2021-11-16 14:28:59 +0700

    counter button

--- app/src/main/java/com/anuchito/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/anuchito/jetpackcompose/MainActivity.kt
@@ -5,11 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,6 +40,17 @@ fun ScreenContent(names: List<String> = listOf("AnuchitO!!", "Nong")) {
             Greeting(name = name)
             Divider()
         }
+        Counter()
+    }
+}
+
+@Composable
+fun Counter() {
+    var count by remember {
+        mutableStateOf(0)
+    }
+    Button(onClick = { count++ }) {
+        Text(text = "I've been clicked $count times.")
     }
 }
 
