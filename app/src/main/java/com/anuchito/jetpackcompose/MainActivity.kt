4be96159ffafca322a4783ef7da2fb8b4dd705f6
AnuchitO <anuchit.prasertasng@gmail.com>
2021-11-16 14:15:47 +0700

    add padding 16.dp

--- app/src/main/java/com/anuchito/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/anuchito/jetpackcompose/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.anuchito.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,7 +47,10 @@ fun ScreenContent() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Voila $name!")
+    Text(
+        text = "Voila $name!",
+        modifier = Modifier.padding(16.dp)
+    )
 }
 
 @Preview(showBackground = true, showSystemUi = true)
